b59b9a85fad0e10814ef34e794f2e7931e6e8229
shourovahmd <moh.abuhanifa@gmail.com>
2023-07-01 02:15:39 +0600

    example router deleted

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
-export default function Home() {
+import { type NextPage } from "next";
+
+const Home: NextPage = () => {
   return (
     <div>
       <div></div>
     </div>
   );
-}
+};
+
+export default Home;
